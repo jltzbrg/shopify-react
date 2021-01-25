f14d3a39a336b79ea50378a4e682a5d9675d2203
Julio Litzenberg <julio.litzenberg@gmail.com>
2021-01-25 18:32:33 +0100

    :poop: Add Context API
    Adding the context API from React, but get an error from the ShopProvider

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
 import Client from 'shopify-buy';
 
+const ShopContext = React.createContext;
+const ShopConsumer = ShopContext.Consumer;
+
 const client = Client.buildClient({
   domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
   storefrontAccessToken: process.env.REACT_APP_SHOPIFY_API,
 });
+class ShopProvider extends Component {
+  state = {
+    product: {},
+    products: [],
+    checkout: {},
+    isCartOpen: false,
+    isMenuOpen: false,
+  };
 
-export class ShopProvider extends Component {
+  componentDidMount() {
+    this.createCheckout();
+  }
+
+  createCheckout = async () => {
+    const checkout = await client.checkout.create();
+    localStorage.setItem('checkout-id', checkout.id);
+    this.setState({ checkout: checkout });
+  };
+
+  fetchCheckout = async () => {};
+
+  addItemtoCheckout = async () => {};
+
+  removeLineItem = async (lineItemIdstoRemove) => {};
+
+  fetchAllProducts = async () => {
+    const products = await client.product.fetchAll();
+    this.setState({ products: products });
+  };
+
+  fetchProductWithHandle = async (handle) => {
+    const product = await client.product.fetchByHandle(handle);
+    this.setState({ product: product });
+  };
+
+  closeCart = () => {};
+
+  openCart = () => {};
+
+  closeMenu = () => {};
+
+  openMenu = () => {};
+
   render() {
-    return <div></div>;
+    console.log(this.state.checkout);
+
+    return <ShopContext.Provider>{this.props.children}</ShopContext.Provider>;
   }
 }
 
-export default shopContext;
+export { ShopConsumer, ShopContext };
+
+export default ShopProvider;
