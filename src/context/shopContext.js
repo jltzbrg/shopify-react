7a75367d0892e3d84a04b34f0c679557f04b088e
Julio Litzenberg <julio.litzenberg@gmail.com>
2021-01-25 14:44:34 +0100

    :fire: Cleanup + Storefront API
    Cleaning the project structure and conneting Shopify Storefront API

---
+++ src/context/shopContext.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Client from 'shopify-buy';
+
+const client = Client.buildClient({
+  domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
+  storefrontAccessToken: process.env.REACT_APP_SHOPIFY_API,
+});
+
+export class ShopProvider extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default shopContext;
