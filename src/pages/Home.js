ac283a8d16f1d316a500d1ff75174b01e385e60e
Julio Litzenberg <julio.litzenberg@gmail.com>
2021-05-17 00:57:22 +0200

    Creating Home Layout for product with chakra-ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from 'react';
 import { ShopContext } from '../context/shopContext';
 import { Link } from 'react-router-dom';
+import { Box, Grid, Text, Image } from '@chakra-ui/react';
 
 function Home() {
   const { fetchAllProducts, products } = useContext(ShopContext);
@@ -9,16 +10,23 @@ function Home() {
     fetchAllProducts();
   }, [fetchAllProducts]);
 
+  console.log(products);
   if (!products) return <div>Loadings....</div>;
 
   return (
-    <div>
-      {products.map((product) => (
-        <Link to={`/products/${product.handle}`} key={product.title}>
-          {product.title}
-        </Link>
-      ))}
-    </div>
+    <Box>
+      <Grid templateColumns='repeat(3, 1fr)'>
+        {products.map((product) => (
+          <Link to={`/products/${product.handle}`} key={product.id}>
+            <Box _hover={{ opacity: '80%' }} textAlign='center'>
+              <Image src={product.images[0].src} />
+              <Text>{product.title}</Text>
+              <Text>${product.variants[0].price}</Text>
+            </Box>
+          </Link>
+        ))}
+      </Grid>
+    </Box>
   );
 }
 
