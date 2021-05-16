647c4c8263e9eaba2e361a328cf1d8a057c371a3
Julio Litzenberg <julio.litzenberg@gmail.com>
2021-05-17 01:25:43 +0200

    Creating the Navigation Component

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { Flex, Icon, Image, Text } from '@chakra-ui/react';
+import ShopContext from '../context/shopContext';
+
+const NavBar = () => {
+  //const { openCart, openMenu, checkout } = useContext(ShopContext);
+  return (
+    <Flex flexDir='row' justifyContent='space-between' p='2rem'>
+      <Text>Menu</Text>
+      <Text>Logo</Text>
+      <Text>Cart</Text>
+    </Flex>
+  );
+};
+
+export default NavBar;
