f8185e8cc24454dd7b0d469672189277819eb74b
Julio Litzenberg <julio.litzenberg@gmail.com>
2021-05-17 01:38:22 +0200

    Styling the navigation component with color, icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,27 @@
 import React, { useContext } from 'react';
 import { Flex, Icon, Image, Text } from '@chakra-ui/react';
 import ShopContext from '../context/shopContext';
-
+import { MdMenu, MdShoppingBasket } from 'react-icons/md';
 const NavBar = () => {
   //const { openCart, openMenu, checkout } = useContext(ShopContext);
   return (
-    <Flex flexDir='row' justifyContent='space-between' p='2rem'>
-      <Text>Menu</Text>
-      <Text>Logo</Text>
-      <Text>Cart</Text>
+    <Flex
+      backgroundColor='#FFA8E2'
+      flexDir='row'
+      justifyContent='space-between'
+      p='2rem'>
+      <Icon fill='white' cursor='pointer' as={MdMenu} w={30} h={30}></Icon>
+      <Image
+        src='https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540'
+        w={100}
+        h={100}
+      />
+      <Icon
+        fill='white'
+        cursor='pointer'
+        as={MdShoppingBasket}
+        w={30}
+        h={30}></Icon>
     </Flex>
   );
 };
