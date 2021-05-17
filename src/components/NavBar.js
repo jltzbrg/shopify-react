ba8b8b5906d371a44e5f04bfecbad917c4cfccb0
Julio Litzenberg <julio.litzenberg@gmail.com>
2021-05-17 12:59:08 +0200

    Redirect user to home when logo is click

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import { Flex, Icon, Image } from '@chakra-ui/react';
 import { ShopContext } from '../context/shopContext';
 import { MdMenu, MdShoppingBasket } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+
 const NavBar = () => {
   const { openCart } = useContext(ShopContext);
 
@@ -12,11 +14,13 @@ const NavBar = () => {
       justifyContent='space-between'
       p='2rem'>
       <Icon fill='white' cursor='pointer' as={MdMenu} w={30} h={30}></Icon>
-      <Image
-        src='https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540'
-        w={100}
-        h={100}
-      />
+      <Link to='/'>
+        <Image
+          src='https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg?v=1610055540'
+          w={100}
+          h={100}
+        />
+      </Link>
       <Icon
         fill='white'
         cursor='pointer'
